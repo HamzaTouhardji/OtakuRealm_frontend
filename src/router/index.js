5ac590f129a144788b50889000ee8d89fe65e73f
Jokaria <47916626+Jokaria@users.noreply.github.com>
2021-04-03 20:57:11 +0200

    test

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
+import Profil from '../views/Profil.vue'
 
 const routes = [
   {
@@ -20,6 +21,12 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: Login
+
+  },
+  {
+    path: '/profil',
+    name: 'Profil',
+    component: Profil
 
   }
 ]
