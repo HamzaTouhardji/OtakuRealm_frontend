b79fe6d475603f1a54f0a06757197e5ce1ed0f8c
noauffal <noauffal@gmail.com>
2021-04-13 17:08:59 +0200

    footer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <Header/>
   <router-view></router-view>
+  <Footer/>
 </template>
 
 <script>
  
 import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 export default {
   name: 'App',
   components:{
-    Header
+    Header,
+    Footer
   },
   methods:{
     getCookie: function(cname) {
