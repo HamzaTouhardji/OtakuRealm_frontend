feb77e99ed0e68d09de5efda083ce68f13dcc4cc
noauffal <noauffal@gmail.com>
2021-04-21 18:48:33 +0200

    Formulaire pour editer son profile dans la pgae profil il manque juste un css que joseph va faire car il fait un master en html et css

--- src/views/Profil.vue
+++ src/views/Profil.vue
@@ -1,45 +1,145 @@
 
 <template>
   <div class="profil">
-
-
-        <div class="featured">
-          <div class="featured-vertical">
-            <div class="featured-horizontal">
-              <div class="featured-content">
-                <div class="featured-content-title">Profil</div>
-                <span class="featured-content-synopsis">
-                  <img class="photo" v-bind:src="userInfo[0].photo_de_profil">
-                  <p>{{userInfo[0].bio}}</p>
-                  <p>Sex: {{userInfo[0].sexe}}</p>
-                  <p>Age: {{userInfo[0].age}}</p>
-                </span>
-                <div class="genres">
-                <div
-                  class="genre"
-                  v-for="item in selected"
-                  :key="item.id"
-                >
-                  <label class="noselect" v-bind:for="item.id_genre.id">{{
-                    item.id_genre.name
-                  }}</label>
-                </div>
+    <div v-if="formulaire" class="featured">
+      <div class="featured-vertical">
+        <div class="featured-horizontal">
+          <div class="featured-content">
+            <div class="form-box">
+              <h1>Edit your profil</h1>
+              <div class="text-box">
+                <i class="fa fa-user" aria-hidden="true"></i>
+                <input
+                  type="text"
+                  placeholder="Username"
+                  name="username"
+                  v-model="user.username"
+                />
               </div>
-              <router-link to="/recommandation" class="menu-link">Modifier</router-link>
+               <div class="text-box">
+                <i class="fa fa-user" aria-hidden="true"></i>
+                <input
+                  type="text"
+                  placeholder="profil picture url"
+                  name="url"
+                  v-model="user.photo_de_profil"
+                />
+              </div>
+              <div class="text-box">
+                <i class="fa fa-user" aria-hidden="true"></i>
+                <input
+                  type="number"
+                  placeholder="Age"
+                  name="Age"
+                  v-model="user.age"
+                />
+              </div>
+              <div class="text-box">
+                <i class="fa fa-user" aria-hidden="true"></i>
+                <select
+                  type="text"
+                  placeholder="Sex"
+                  name="username"
+                  v-model="user.sexe"
+                >
+                  <option selected value="M" v-if="'M' == user.sexe">
+                    Male
+                  </option>
+                  <option v-else value="M">Male</option>
+
+                  <option selected value="F" v-if="'F' == user.sexe">
+                    Female
+                  </option>
+                  <option v-else value="F">Female</option>
+                </select>
+              </div>
+              <div class="text-box">
+                <i class="fa fa-user" aria-hidden="true"></i>
+                <input
+                  type="text"
+                  placeholder="Bio"
+                  name="bio"
+                  v-model="user.bio"
+                />
+              </div>
+
+              <div class="buttons">
+                <input
+                  class="btn"
+                  type="button"
+                  name=""
+                  value="Save changes"
+                  @click="editProfil"
+                />
+                <input
+                  class="btn"
+                  type="button"
+                  name=""
+                  value="Cancel"
+                  @click="formulaire = false"
+                />
               </div>
             </div>
           </div>
         </div>
+      </div>
+    </div>
 
+    <div v-else class="featured">
+      <div class="featured-vertical">
+        <div class="featured-horizontal">
+          <div class="featured-content">
+            <div class="featured-content-title">Profil</div>
+            <span class="featured-content-synopsis">
+              <img class="photo" v-bind:src="userInfo[0].photo_de_profil" />
+              <p>{{ userInfo[0].bio }}</p>
+              <p>Sex: {{ userInfo[0].sexe }}</p>
+              <p>Age: {{ userInfo[0].age }}</p>
+            </span>
+            <div class="genres">
+              <div class="genre" v-for="item in selected" :key="item.id">
+                <label class="noselect" v-bind:for="item.id_genre.id">{{
+                  item.id_genre.name
+                }}</label>
+              </div>
+            </div>
+            <div class="buttons" style="width: 300px">
+              <router-link class="link" to="/recommandation"
+                ><input
+                  class="btn"
+                  type="button"
+                  name=""
+                  value="Edit genres"
+                  @click="formulaire = false"
+              /></router-link>
 
+              <input
+                class="btn"
+                type="button"
+                name=""
+                value="Edit profil"
+                @click="formulaire = true"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
     <div class="featured-content">
       <div class="featured-content-title">Watchlist</div>
     </div>
-    <Carousel :settings="settings" :breakpoints="breakpoints" :key="selectedAnime">
+    <Carousel
+      :settings="settings"
+      :breakpoints="breakpoints"
+      :key="selectedAnime"
+    >
       <Slide v-for="anime in selectedAnime" :key="anime">
         <div class="carousel__item">
-          <a v-bind:href="'#/detailanime?id=' + anime.id_anime.id" draggable="false">
+          <a
+            v-bind:href="'#/detailanime?id=' + anime.id_anime.id"
+            draggable="false"
+          >
             <img
               v-bind:src="anime.id_anime.URL"
               v-bind:alt="anime.id_anime.title"
@@ -72,9 +172,11 @@ export default defineComponent({
     Navigation,
   },
   data: () => ({
-    userInfo:[{}],
+    formulaire: false,
+    userInfo: [{}],
     selected: [],
     selectedAnime: [],
+    user: {},
     // carousel settings
     settings: {
       itemsToShow: 1,
@@ -108,13 +210,13 @@ export default defineComponent({
     await this.getGenreUser();
   },
   methods: {
-    getCookie: function(cname) {
+    getCookie: function (cname) {
       var name = cname + "=";
       var decodedCookie = decodeURIComponent(document.cookie);
-      var ca = decodedCookie.split(';');
-      for(var i = 0; i <ca.length; i++) {
+      var ca = decodedCookie.split(";");
+      for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
-        while (c.charAt(0) == ' ') {
+        while (c.charAt(0) == " ") {
           c = c.substring(1);
         }
         if (c.indexOf(name) == 0) {
@@ -123,51 +225,135 @@ export default defineComponent({
       }
       return "";
     },
-    async getInfoUser(){
-   
-      let headers = {"Content-Type": "application/json"};
-      console.log(this.getCookie('token'));
-      headers["Authorization"] = `Token ` + this.getCookie('token');
-      var response = await fetch('http://otakurealm.mooo.com/api/info_utilisateur/',{headers})
+    async getInfoUser() {
+      let headers = { "Content-Type": "application/json" };
+
+      headers["Authorization"] = `Token ` + this.getCookie("token");
+      var response = await fetch(
+        "http://otakurealm.mooo.com/api/info_utilisateur/",
+        { headers }
+      );
       this.userInfo = await response.json();
+      this.user = this.userInfo[0];
       //console.log(this.userInfo)
     },
     async getAnimeUser() {
       let headers = { "Content-Type": "application/json" };
-      console.log(this.getCookie("token"));
+
       headers["Authorization"] = `Token ` + this.getCookie("token");
       var response = await fetch(
         "http://otakurealm.mooo.com/api/anime/utilisateur",
         { headers }
       );
       this.selectedAnime = await response.json();
-      //console.log(this.selectedAnime)
-      
     },
     async getGenreUser() {
       let headers = { "Content-Type": "application/json" };
-      console.log(this.getCookie("token"));
+
       headers["Authorization"] = `Token ` + this.getCookie("token");
       var response = await fetch(
         "http://otakurealm.mooo.com/api/genre/utilisateur",
         { headers }
       );
       this.selected = await response.json();
-      console.log(this.selected);
     },
-  }
+    editProfil: function () {
+      let headers = { "Content-Type": "application/json" };
+
+      headers["Authorization"] = `Token ` + this.getCookie("token");
+
+      fetch("http://otakurealm.mooo.com/api/info_utilisateur/", {
+        method: "PUT",
+        headers,
+        body: JSON.stringify(this.user),
+      }).then(
+        function (response) {
+          if (response.status === 200) {
+            return response.json();
+          } else {
+            console.log("erreur requete");
+            return null;
+          }
+        },
+        function (err) {
+          console.log("err", err);
+        }
+      );
+      this.formulaire = false;
+    },
+  },
 });
 </script>
 <style scoped>
+option {
+  background-color: #111;
+  color: white;
+}
 
+.buttons {
+  display: flex;
+}
+
+.form-box {
+  width: 280px;
+
+  color: white;
+}
+
+.form-box h1 {
+  font-size: 32px;
+  border-bottom: 6px solid #1e1e33;
+  margin-bottom: 50px;
+  padding: 13px 0px;
+}
+.link {
+  width: 100%;
+}
+.text-box {
+  width: 100%;
+  overflow: hidden;
+  font-size: 20px;
+  padding: 8px 0;
+  margin: 8px 0;
+  border-bottom: 1px solid #1e1e33;
+}
+
+.text-box i {
+  width: 26px;
+  float: left;
+  text-align: center;
+}
+
+.text-box input,
+select {
+  border: none;
+  outline: none;
+  background: none;
+  color: white;
+  font-size: 18px;
+  width: 80%;
+  float: left;
+  margin: 0 10px;
+}
+
+.btn {
+  width: 100%;
+  background: none;
+  border: 2px solid #1e1e33;
+  color: white;
+  padding: 5px;
+  font-size: 18px;
+  cursor: pointer;
+  margin: 12px 0;
+}
 .profil {
   background-color: rgba(128, 128, 128, 0.021);
-  margin : auto;
-  width : 70%;
+  margin: auto;
+  width: 70%;
   margin-top: 5vw;
   margin-bottom: 5vw;
   padding-bottom: 5vw;
-  border : 1px solid;
+  border: 1px solid;
   border-color: rgba(255, 255, 255, 0.096);
   border-radius: 30px;
   box-shadow: 5px 5px 50px rgba(255, 255, 255, 0.096);
